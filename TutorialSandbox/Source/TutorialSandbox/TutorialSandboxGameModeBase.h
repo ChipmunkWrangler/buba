5ab1338c52260b097f1fb5604f4025cfede9399c
Rafael <>
2018-08-06 09:41:48 +0200

    Add generated C++ skeleton.

---
+++ TutorialSandbox/Source/TutorialSandbox/TutorialSandboxGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TutorialSandboxGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TUTORIALSANDBOX_API ATutorialSandboxGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
